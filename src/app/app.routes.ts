995ef8b11518c9e99db857e26fdb07054f4749d3
leonel Marquez <leomarqz2020@gmail.com>
2025-07-31 14:26:13 -0600

    feature: Se configuraron todas las rutas posibles de la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,15 +8,23 @@ import { ListCinesComponent } from './cines/list-cines/list-cines.component';
 import { CreateCineComponent } from './cines/create-cine/create-cine.component';
 import { ListActorsComponent } from './actors/list-actors/list-actors.component';
 import { CreateActorComponent } from './actors/create-actor/create-actor.component';
+import { EditGenreComponent } from './genres/edit-genre/edit-genre.component';
+import { EditCineComponent } from './cines/edit-cine/edit-cine.component';
+import { EditActorComponent } from './actors/edit-actor/edit-actor.component';
+import { EditMovieComponent } from './movies/edit-movie/edit-movie.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'cines', component: ListCinesComponent},
     {path: 'cines/create', component: CreateCineComponent},
+    {path: 'cines/edit/:id', component: EditCineComponent},
     {path: 'genres', component: ListGenresComponent},
     {path: 'genres/create', component: CreateGenreComponent},
+    {path: 'genres/edit/:id', component: EditGenreComponent},
     {path: 'actors', component: ListActorsComponent},
     {path: 'actors/create', component: CreateActorComponent},
-    // {path: 'movies', component: ListMoviesComponent},
+    {path: 'actors/edit/:id', component: EditActorComponent},
+    {path: 'movies', component: ListMoviesComponent},
     {path: 'movies/create', component: CreateMovieComponent},
+    {path: 'movies/edit/:id', component: EditMovieComponent}
 ];
---
+++ src/app/cines/edit-cine/edit-cine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cine',
+  imports: [],
+  templateUrl: './edit-cine.component.html',
+  styleUrl: './edit-cine.component.css'
+})
+export class EditCineComponent {
+
+}
